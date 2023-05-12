603c187d2afc5df0b356f273489bace5b5d81b6d
muhamadjukahpin <muhamadjukahpin@gmail.com>
2023-05-12 21:23:02 +0700

    commit pertama

---
+++ README.md
@@ -0,0 +1,29 @@
+# coffee-delivery-app
+
+## Get Started
+
+install dev dependencies terlebih dahulu
+
+### `npm install` or `yarn install`
+
+## Kemudian
+
+Jalankan aplikasi
+
+\*Jangan lupa untuk download Expo go di emulator anda atau di ponsel anda masing - masing.
+
+### `npm start` or `yarn start`
+
+Menjalankan aplikasi Anda dalam mode pengembangan.
+
+Buka di [aplikasi Expo](https://expo.io) di ponsel Anda untuk melihatnya. Ini akan dimuat ulang jika Anda menyimpan hasil edit ke file Anda, dan Anda akan melihat kesalahan build dan log di terminal.
+
+#### `npm run ios` or `yarn run ios`
+
+Seperti `npm start` / `yarn start`, tetapi juga mencoba membuka aplikasi Anda di Simulator iOS jika Anda menggunakan Mac dan telah menginstalnya.
+
+#### `npm run android` or `yarn run android`
+
+Seperti `npm start` / `yarn start`, tetapi juga mencoba membuka aplikasi Anda di perangkat Android atau emulator yang terhubung. Memerlukan penginstalan alat build Android (lihat [React Native docs](https://facebook.github.io/react-native/docs/getting-started.html) untuk penyiapan mendetail).
+
+<br />
---
+++ constants/index.js
@@ -0,0 +1,78 @@
+// we will be using this dummy data,
+// don't worry, you can find this data in the git repo
+// you can find the git repo link in the description ;)
+
+export const categories = [
+  {
+    id: 1,
+    title: "Cappuccino",
+  },
+  {
+    id: 2,
+    title: "Latte",
+  },
+  {
+    id: 3,
+    title: "Espresso",
+  },
+  {
+    id: 4,
+    title: "Mocha",
+  },
+  {
+    id: 5,
+    title: "Americano",
+  },
+];
+
+export const coffeeItems = [
+  {
+    id: 1,
+    name: "Black Coffee",
+    price: "25.000",
+    volume: "116 ml",
+    stars: "4.6",
+    image: require("../assets/images/coffee1.png"),
+    desc: "Rasa kopi dapat bervariasi tergantung pada faktor-faktor seperti jenis biji, tingkat pemanggangan, metode penyeduhan, dan penambahan perasa atau pemanis.",
+  },
+
+  {
+    id: 2,
+    name: "Cappuccino",
+    price: "15.500",
+    volume: "110 ml",
+    stars: "4.3",
+    image: require("../assets/images/coffee2.png"),
+    desc: "Rasa kopi dapat bervariasi tergantung pada faktor-faktor seperti jenis biji, tingkat pemanggangan, metode penyeduhan, dan penambahan perasa atau pemanis.",
+  },
+
+  {
+    id: 3,
+    name: "Espresso",
+    price: "30.000",
+    volume: "100 ml",
+    stars: "4.0",
+    image: require("../assets/images/coffee3.png"),
+    desc: "Rasa kopi dapat bervariasi tergantung pada faktor-faktor seperti jenis biji, tingkat pemanggangan, metode penyeduhan, dan penambahan perasa atau pemanis.",
+  },
+
+  {
+    id: 4,
+    name: "Latte",
+    price: "10.300",
+    volume: "80 ml",
+    stars: "3.5",
+    image: require("../assets/images/coffee4.png"),
+    desc: "Rasa kopi dapat bervariasi tergantung pada faktor-faktor seperti jenis biji, tingkat pemanggangan, metode penyeduhan, dan penambahan perasa atau pemanis.",
+  },
+
+  {
+    id: 5,
+    name: "Mocha",
+    price: "23.100",
+    volume: "120 ml",
+    stars: "4.7",
+    image: require("../assets/images/coffee5.png"),
+    desc: "Rasa kopi dapat bervariasi tergantung pada faktor-faktor seperti jenis biji, tingkat pemanggangan, metode penyeduhan, dan penambahan perasa atau pemanis.",
+  },
+];
